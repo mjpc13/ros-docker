b0439d27ee8025470617646dba88ec599ba82065
mjpc13 <mjpc13@protonmail.com>
2022-09-22 15:39:41 +0100

    Remove source of catkin workspace

--- catkin_entrypoint.sh
+++ catkin_entrypoint.sh
@@ -4,8 +4,5 @@ echo "==> Executing image entrypoint ..."
 echo "-> Setting up ROS"
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 
-echo "-> Setting up catkin workspace"
-source "$CATKIN_WS/devel/setup.bash"
-
 echo "==> Container ready"
 exec "$@"
